610ac167b7e190b034d58521337e7ba54a4014f2
OhMyDitzzy <dawaifajar3@gmail.com>
2026-08-20 15:51:20 +0700

    fix: Makefiles configuration

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_dew.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_pond-user \
-    twrp_pond-userdebug \
-    twrp_pond-eng
+    twrp_dew-user \
+    twrp_dew-userdebug \
+    twrp_dew-eng
